ee7ebb47a6e1812858c8ab5809075a60662c600b
Uli Heller <uli.heller@daemons-point.com>
2013-09-05 17:37:04 +0200

    Aenderungen via MD5

--- create-galleries.sh
+++ create-galleries.sh
@@ -5,17 +5,51 @@ D="$(dirname "$0")"
 GALLERIES=source/images/galleries
 THUMBNAILS=thumbs
 SCALED=scaled
+MD5=_md5
+# Names which start with an underscore '_' are not copied into public my jekyll/octopress
 
 THUMBNAIL_GEOMETRY=64x48
 SCALED_GEOMETRY=640x480
+
+calc_md5() {
+    cat "$1"|md5sum|(read a b; echo "$a")
+}
+
+md5name() {
+  (
+    srcImage="$1"
+    dir="$(dirname "${srcImage}")"
+    base="$(basename "${srcImage}")"
+    echo "${dir}/${MD5}/${base}.md5"
+  )
+}
+
+create_md5() {
+  (
+    srcImage="$1"
+    dstMd5="$(md5name "${srcImage}")"
+    calc_md5 "${srcImage}" >"${dstMd5}"
+  )
+}
+
+check_md5() {
+  (
+    srcImage="$1"
+    srcMd5="$(md5name "${srcImage}")"
+    test -s "${srcMd5}" && test "$(calc_md5 "${srcImage}")" = "$(cat "${srcMd5}")"
+  )
+}
 
 create_image() {
   (
     srcImage="$1"
     dstImage="$2"
     shift 2
-    if [ "${srcImage}" -nt "${dstImage}" ]; then
-      convert "$@" "${srcImage}" "${dstImage}"
+    if ! check_md5 "${srcImage}" "$(md5name  "${srcImage}")"; then
+      (
+        set -x
+        convert "$@" "${srcImage}" "${dstImage}"
+      )
     fi
   )
 }
@@ -34,7 +68,7 @@ create_scaled() {
 
 for gallery in "${D}/${GALLERIES}/"*; do
   echo $gallery;
-  for d in "${gallery}/${THUMBNAILS}" "${gallery}/${SCALED}"; do
+  for d in "${gallery}/${MD5}" "${gallery}/${THUMBNAILS}" "${gallery}/${SCALED}"; do
     if [ ! -d "${d}" ]; then
       mkdir -p "${d}"
     fi
@@ -44,6 +78,7 @@ for gallery in "${D}/${GALLERIES}/"*; do
       base_image="$(basename "${image}")"
       create_thumbnail "${image}" "${gallery}/${THUMBNAILS}/${base_image}"
       create_scaled "${image}" "${gallery}/${SCALED}/${base_image}"
+      create_md5 "${image}" "${gallery}/${MD5}/${base_image}.md5"
     fi
   done
 done
